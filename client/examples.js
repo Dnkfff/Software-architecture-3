a54cea126417731c6a08df59090f2f48539aa4f9
Danil <ddynikov@gmail.com>
2021-12-02 21:56:06 +0200

    second task done

--- client/examples.js
+++ client/examples.js
@@ -4,23 +4,23 @@ const channels = import('./menus/client.js');
 
 const client = channels.then((result) => result.Client('http://localhost:8080'));
 
-// Scenario 1: Display the menu.
-// client.then((obj) => {
-//       console.log('Scenario 1');
-//       console.log('Available menu:');
-//       return obj.listMenu();
-//     }).then((result) => console.log(result))
-//     .catch((e) => {
-//         console.log(`Problem listing available menu: ${e.message}`);
-//     });
+//Scenario 1: Display the menu.
+client.then((obj) => {
+      console.log('Scenario 1');
+      console.log('Available menu:');
+      return obj.listMenu();
+    }).then((result) => console.log(result))
+    .catch((e) => {
+        console.log(`Problem listing available menu: ${e.message}`);
+    });
 
 // Scenario 2: Creating an order.
 
 client.then((obj) => {
-         const createOrder = obj.createOrder(0,1);
+         const createOrder = obj.createOrder(0,1); // outputing order 0, 1
          console.log('Scenario 2');
          console.log('Creating an order from menu:');
-         console.log(obj)
+         createOrder.then((result) => console.log(result))
      })
      .catch((e) => {
          console.log(`Problem creating a new order: ${e.message}`);
